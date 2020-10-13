ffa91850a3bdc6d492250be034a179fb89f6e7b7
kshamko <konstantin.shamko@gmail.com>
2020-10-13 18:57:19 +0300

    sample code for week1

---
+++ week1/week1.py
@@ -0,0 +1,16 @@
+
+balance = 100
+print(balance)
+
+balance = balance - 5
+print(balance)
+
+amount = int(input())
+print(type(amount))
+
+if balance >= amount:
+    balance = balance - amount
+else:
+    print("#!@#$%$!@#$!")    
+
+print(balance)    
